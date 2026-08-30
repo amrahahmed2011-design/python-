46a98da61035836bf44aee4a22d99e08d5d621b1
amrahahmed2011-design <amrahahmed2011@gmail.com>
2026-08-30 21:10:47 +0500

    Add files via upload

---
+++ MATHS OPERATIONS.py
@@ -0,0 +1,8 @@
+import math
+print("THE FLOOR AND CEILING VALUE OF 58.78 ARE " + str(math.ceil(58.78))+"," +str(math.floor(58.78)))
+
+x = 376
+y= -632
+print("THE VALUE OF X AFTER COPYING THE SIGN FROM Y IS:" + str(math.copysign(x,y)))
+print("THE ABSOLUTE VALUE OF -789 AND 656"+ str(math.fabs(-789))+ ","+ str(math.fabs(656)))
+print("THE GCD OF 124 AND 256 IS:" + str(math.gcd(124,256)))
