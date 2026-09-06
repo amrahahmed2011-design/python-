944f807a811794c1b9b50ea2107e6747602efaa4
amrahahmed2011-design <amrahahmed2011@gmail.com>
2026-09-06 20:19:05 +0500

    Add files via upload

---
+++ LISTS.py
@@ -0,0 +1,13 @@
+#CREATE AN EMPTY LIST
+empty_list = []
+print()
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triples = [1,3,7]*3
+print(triples)
+
+aList = [1, 2, 3, 4, 5]
+aList = aList[::-1]
+print(aList)
