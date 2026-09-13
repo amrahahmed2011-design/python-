7bb7cc9c45f6150b8ce61725abc3989956d616a4
amrahahmed2011-design <amrahahmed2011@gmail.com>
2026-09-13 20:54:26 +0500

    Add files via upload

---
+++ GET THE COUNTRY CODE.py
@@ -0,0 +1,26 @@
+country_code ={
+    "Afghanistan": "+93",
+    "PAKISTAN": "+92",
+    "US": "+1",
+    "INDIA": "+91",
+    "CHINA": "+86",
+    "OMAN": "+968",
+    "NEPAL": "+977",
+    "FRANCE": "+33",
+    "ITALY": "+39",
+
+}
+print("THE COUNTRY CODE FOR PAKISTAN IS -")
+print(country_code.get("PAKISTAN", "NOT FOUND"))
+
+print("THE COUNTRY CODE FOR IRAN IS -")
+print(country_code.get("IRAN", "NOT FOUND"))
+
+print("THE COUNTRY CODE FOR CHINA IS -")
+print(country_code.get("CHINA", "NOT FOUND"))
+
+print("THE COUNTRY CODE FOR KOREA IS -")
+print(country_code.get("KOREA", "NOT FOUND"))
+
+print("THE COUNTRY CODE FOR INDIA IS -")
+print(country_code.get("INDIA", "NOT FOUND"))
