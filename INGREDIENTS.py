08bc6735d9474afd5966450ed69e903287aedc32
amrahahmed2011-design <amrahahmed2011@gmail.com>
2026-09-19 20:57:53 +0500

    Add files via upload

---
+++ INGREDIENTS.py
@@ -0,0 +1,6 @@
+pasta = {"pasta","milk","salt","pepper","spices","carrots","corn","cheese"}
+dessert = {"rusk","milk","chocolate","sugar","cream"}
+common = pasta.intersection(dessert)
+print(common)
+total = pasta.union (dessert)
+print(total)
